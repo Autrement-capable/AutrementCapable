b85b2330f5c40075cfc72d615d2a54104365f977
Eliyan <eliyan.dochev@epitech.eu>
2024-07-04 16:05:30 +0200

    [ADD] ability to download cv

--- website/src/components/CvPreview.vue
+++ website/src/components/CvPreview.vue
@@ -7,6 +7,7 @@
       class="full-size-image"
     />
     <p v-else>Loading image...</p>
+    <button @click="fetchPdf" class="download-button">Download PDF</button>
   </div>
 </template>
 
@@ -34,6 +35,31 @@ export default {
           console.error("Error fetching image:", error);
         });
     },
+    fetchPdf() {
+      // Replace with your API endpoint for the PDF
+      const pdfUrl = "http://127.0.0.1:5000/cv_download";
+
+      fetch(pdfUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.blob();
+        })
+        .then((blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.style.display = "none";
+          a.href = url;
+          a.download = "document.pdf"; // Set the file name
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+        })
+        .catch((error) => {
+          console.error("Error fetching PDF:", error);
+        });
+    },
   },
 };
 </script>
@@ -41,13 +67,21 @@ export default {
 <style>
 .image-container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
 }
 
 .full-size-image {
-  max-width: 100%;
-  max-height: 100%;
+  max-width: 80%;
+  max-height: 80%;
+}
+
+.download-button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
 }
 </style>
