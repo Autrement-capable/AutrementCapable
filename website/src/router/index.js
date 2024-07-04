ff8dfbd922672344138dff76a97904220d7c880d
Eliyan <eliyan.dochev@epitech.eu>
2024-07-04 13:20:41 +0200

    [ADD] cv preview frontend and  backend

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,65 +1,71 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import StartButton from '../components/StartButton.vue'
-import AccountCreation from '../components/AccountCreation.vue'
-import Explanation from '../components/Explanation.vue'
-import PersonalTest from '../components/PersonalTest.vue'
-import UserQuestionnaire from '../components/Questionnaire.vue'
-import GameSpeed from '../components/GameSpeed.vue'
-import GameShape from '../components/GameShape.vue'
-import GameMemory from '../components/GameMemory.vue'
-import CompDashboard from '../components/CompDashboard.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import StartButton from "../components/StartButton.vue";
+import AccountCreation from "../components/AccountCreation.vue";
+import Explanation from "../components/Explanation.vue";
+import PersonalTest from "../components/PersonalTest.vue";
+import UserQuestionnaire from "../components/Questionnaire.vue";
+import GameSpeed from "../components/GameSpeed.vue";
+import GameShape from "../components/GameShape.vue";
+import GameMemory from "../components/GameMemory.vue";
+import CompDashboard from "../components/CompDashboard.vue";
+import CvPreview from "../components/CvPreview.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'StartButton',
-    component: StartButton
+    path: "/",
+    name: "StartButton",
+    component: StartButton,
   },
   {
-    path: '/account-creation',
-    name: 'AccountCreation',
-    component: AccountCreation
+    path: "/account-creation",
+    name: "AccountCreation",
+    component: AccountCreation,
   },
   {
-    path: '/explanation',
-    name: 'Explanation',
-    component: Explanation
+    path: "/explanation",
+    name: "Explanation",
+    component: Explanation,
   },
   {
-    path: '/personal-test',
-    name: 'PersonalTest',
-    component: PersonalTest
+    path: "/personal-test",
+    name: "PersonalTest",
+    component: PersonalTest,
   },
   {
-    path: '/questionnaire',
-    name: 'Questionnaire',
-    component: UserQuestionnaire
+    path: "/questionnaire",
+    name: "Questionnaire",
+    component: UserQuestionnaire,
   },
   {
-    path: '/game-speed',
-    name: 'GameSpeed',
-    component: GameSpeed
+    path: "/game-speed",
+    name: "GameSpeed",
+    component: GameSpeed,
   },
   {
-    path: '/game-shape',
-    name: 'GameShape',
-    component: GameShape
+    path: "/game-shape",
+    name: "GameShape",
+    component: GameShape,
   },
   {
-    path: '/game-memory',
-    name: 'GameMemory',
-    component: GameMemory
+    path: "/game-memory",
+    name: "GameMemory",
+    component: GameMemory,
   },
   {
-    path: '/dashboard',
-    name: 'CompDashboard',
-    component: CompDashboard
-  }
-]
+    path: "/dashboard",
+    name: "CompDashboard",
+    component: CompDashboard,
+  },
+  {
+    path: "/cv-preview",
+    name: "CvPreview",
+    component: CvPreview,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
