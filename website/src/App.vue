077fc297495715809ff4afe9af077df8bae730ca
Zaharia Vlad <vlad.zaharia@epitech.eu>
2024-10-11 19:35:38 +0200

    [ADD] inclusive widget

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app" class="app-container">
     <router-view></router-view>
+    <div class="accessibility-widget" @click="toggleWidget">
+      <i class="icon-accessibility"></i>
+    </div>
     <AppFooter />
+    <div v-if="showWidget" class="accessibility-options">
+      <!-- Add your accessibility options here -->
+      <button @click="toggleTextToSpeech">Lire la page</button>
+      <button @click="toggleVoiceNavigation">Navigation vocale</button>
+      <!-- Other options -->
+    </div>
   </div>
 </template>
 
@@ -12,29 +21,63 @@ export default {
   name: 'App',
   components: {
     AppFooter
+  },
+  data() {
+    return {
+      showWidget: false
+    };
+  },
+  methods: {
+    toggleWidget() {
+      this.showWidget = !this.showWidget;
+    },
+    toggleTextToSpeech() {
+      // Add text-to-speech functionality here
+    },
+    toggleVoiceNavigation() {
+      // Add voice navigation functionality here
+    }
   }
-}
+};
 </script>
 
 <style>
-@font-face {
-  font-family: 'Glacial Indifference';
-  src: url('@/assets/fonts/GlacialIndifference-Regular.otf') format('opentype');
-  font-weight: normal;
+.accessibility-widget {
+  position: absolute;
+  bottom: 80px; /* This ensures it's above the footer */
+  right: 20px;
+  background-color: #007BFF;
+  color: white;
+  padding: 10px;
+  border-radius: 50%;
+  cursor: pointer;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
 
-@font-face {
-  font-family: 'Glacial Indifference';
-  src: url('@/assets/fonts/GlacialIndifference-Bold.otf') format('opentype');
-  font-weight: bold;
+.accessibility-options {
+  position: absolute;
+  bottom: 140px; /* Slightly higher to make room for the widget itself */
+  right: 20px;
+  background-color: #FFF;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 
-body {
-  font-family: 'Glacial Indifference', sans-serif;
-  padding-top: 0%;
-  margin-top: 0%;
+button {
+  padding: 10px;
+  background-color: #007BFF;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
-.app-container {
-  padding-bottom: 60px;
+
+button:hover {
+  background-color: #0056b3;
 }
+
 </style>
